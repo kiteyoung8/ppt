60ea032d75c401239f6cd5e973733fd8a22e9118
kiteyoung8 <kiteyoung8@gmail.com.com>
2026-02-25 13:37:43 +0800

    Fix: 設定 Tailwind CSS 讓畫面恢復漂亮排版

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // 👈 必須要有這一行，樣式才會載入！
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 這行最重要！告訴 Tailwind 去 src 資料夾找樣式
   ],
   theme: {
     extend: {},
